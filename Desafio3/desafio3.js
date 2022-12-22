e819be8c7605b0a889f2cf527c238f4aa1f12418
Rodrigo <115577327+RodrigoRamirez1@users.noreply.github.com>
2022-12-21 21:09:18 -0300

    alteração 2

--- Desafio3/desafio3.js
+++ Desafio3/desafio3.js
@@ -2,12 +2,33 @@ window.alert("Olá, espero que responda as perguntas com toda a sinceridade do m
 
 var resposta = window.prompt("Você gostaria de seguir para área de 'Front-end' (digite 1) ou para área de 'Back-end?' (digite 2)");
 
-if (resposta == '1') {
-    resposta = window.prompt('Você gostaria de aprender React ou Vue ?') 
+    if (resposta == '1') {
+    linguagem = window.prompt('Você gostaria de aprender React ou Vue ?') 
 
 }
-else {
+
+    else {
     if (resposta == '2') {
-    window.prompt('Você gostaria de aprender C# ou Java?')
+    linguagem = window.prompt('Você gostaria de aprender C# ou Java?')
     }
+}
+
+else {
+    alert('Você não inseriu um dado válido!')
+}
+
+var especializacaoOuFullstack = prompt ('Digite 1 para seguir se especializando na área escolhida ou 2 para seguir se desenvolvendo para se tornar Fullstack')
+
+    if (especializacaoOuFullstack == 1) {
+    alert(`Continue se especializando em ${linguagem} para dominar a área de ${resposta}!`)
+}
+
+    else {
+    if (especializacaoOuFullstack == 2) {
+    alert(`Chegou a hora de começar a aprender outras linguagens além de ${linguagem} se você quer se tornar Fullstack!`)
+    }
+}
+
+else {
+    alert('Você não inseriu um dado válido!')
 }
